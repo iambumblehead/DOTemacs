5631bc76b325ec791488fdd907129184368dfa30
chris <chris@bumblehead.com>
2013-03-31 20:24:03 -0700

    mv .emacs DOTemacs.el

--- DOTemacs.el
+++ DOTemacs.el
@@ -13,13 +13,31 @@
 (add-to-list 'load-path *emacs-js2-mode-path*)
 (add-to-list 'load-path *emacs-json-mode-path*)
 
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(require 'ansi-color)
+
+(defadvice display-message-or-buffer (before ansi-color activate)
+  "Process ANSI color codes in shell output."
+  (let ((buf (ad-get-arg 0)))
+    (and (bufferp buf)
+         (string= (buffer-name buf) "*Shell Command Output*")
+         (with-current-buffer buf
+           (ansi-color-apply-on-region (point-min) (point-max))))))
+
+(defun colorize-compilation-buffer ()
+  "Process ANSI color codes in compilation output."
+  (toggle-read-only)
+  (ansi-color-apply-on-region (point-min) (point-max))
+  (toggle-read-only))
+
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
 
 ;; appearance/theme
 (require 'linum)
-(require 'color-theme)
-(setq color-them-is-global t)
-(color-theme-initialize)
-(load "color-theme-tangotango")(color-theme-tangotango)
+(load-theme 'tsdh-dark t)
 (set-default-font "Liberation Mono-12")
 
 ;; highlight the current line; set a custom face
